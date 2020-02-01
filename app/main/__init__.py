ac3ca7a266c4500f0d49917a9cb79afe426e80b5
moringaschool-Winnie <mwikaliwinnie303@gmail.com>
2020-02-01 15:34:18 +0300

    added view route function that returns index page

---
+++ app/main/__init__.py
@@ -0,0 +1,4 @@
+from flask import Flask
+#Initializing application
+app = Flask(__name__)
+from app import views
---
+++ app/main/views.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from . import main
+from ..request import get_news,get_details
+
+#Views
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    political_list = get_news('kenya','political')
+    business_list = get_news('kenya','business')
+    sports_list = get_news ('kenya','sports')
+    econimy_list = get_news('kenya','economy')
+    technology_list = get_news('kenya','technology')
+    return render_template('index.html',political=political_list,business=business_list,sports=sports_list,economy=economy_list,technology=technology_list)
+@main.route('/news<id>')
+def news(id):
+    """
+    View articles that returns news article from a highlight
+    """
+    news_args = get_details(id)
+    highlight_args = 'Route Working' 
+    # name = k'{results_list}'
+    return render_template('news.html',highlight_param=highlight_args,news=news_args)   

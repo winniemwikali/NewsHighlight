c4ebf8429b7bbbee524a414af70a63101383bbb8
moringaschool-Winnie <mwikaliwinnie303@gmail.com>
2020-02-02 11:45:01 +0300

    added my api to config.py instance

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,3 +2,4 @@ from flask import Flask
 #Initializing application
 app = Flask(__name__)
 from app import views
+from app import error
---
+++ instance/config.py
@@ -0,0 +1,2 @@
+NEWS_API_KEY = 'cbc8be6a9903427d9ebda4efa81fc3a3'
+SECRET_KEY="news"
